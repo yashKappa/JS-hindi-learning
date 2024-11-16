85d5db5282e83fd99c417b3e0d85047997727623
yashKappa <130564276+yashKappa@users.noreply.github.com>
2024-11-16 14:39:18 +0000

    const, var, let

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "yash@google.com"
+var accountPassword = "12345" // Prefer not to use Var, because of issues in block and functional scope 
+accountCity = "Mumbai"
+let accountState = ""
+
+//accountId = 2 // not allowed and cannot be change
+
+accountEmail = "omkar@hoogle.com"
+accountPassword = "678910"
+accountCity = "kokan"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
